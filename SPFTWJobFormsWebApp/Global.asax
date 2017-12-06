82b099477df2ccec49f25a1175c796777227b11c
Laurence Cramp <larcramp@yahoo.ie>
2017-12-06 04:33:17 +0000

    Add project files.

---
+++ SPFTWJobFormsWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SPFTWJobFormsWebApp.MvcApplication" Language="C#" %>
---
+++ SPFTWJobFormsWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SPFTWJobFormsWebApp.Startup))]
+namespace SPFTWJobFormsWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
