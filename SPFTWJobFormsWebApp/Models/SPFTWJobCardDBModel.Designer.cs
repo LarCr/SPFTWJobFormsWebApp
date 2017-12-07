81e99db2d0197effdf2031c4379831475f0683c8
Laurence Cramp <larcramp@yahoo.ie>
2017-12-07 20:54:11 +0000

    Data stuff

--- SPFTWJobFormsWebApp/Models/SPFTWJobCardDBModel.Designer.cs
+++ SPFTWJobFormsWebApp/Models/SPFTWJobCardDBModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'c:\users\sector7g\source\repos\SPFTWJobFormsWebApp\SPFTWJobFormsWebApp\Models\SPFTWJobCardDBModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Sector7G\source\repos\SPFTWJobFormsWebApp\SPFTWJobFormsWebApp\Models\SPFTWJobCardDBModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
